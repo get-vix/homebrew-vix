66a51ede83ca4e839c0185cdf9185585024b88de
Jul <kirby88@pm.me>
2026-06-01 09:27:29 +0100

    vix v0.3.1

--- Formula/vix.rb
+++ Formula/vix.rb
@@ -7,18 +7,18 @@ class Vix < Formula
   on_macos do
     on_arm do
       url "https://github.com/kirby88/vix-releases/releases/download/v0.3.1/vix-darwin-arm64.tar.gz"
-      sha256 "c4c817cb37e8dd6618df3bfe39d33030a63f4ec94a16b8aca2efdba0275fc4b5"
+      sha256 "9a0b5e064559b6f7bdf7e6fb1001580edc28dacf6e1d0c773fcdb52f82d55358"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/kirby88/vix-releases/releases/download/v0.3.1/vix-linux-arm64.tar.gz"
-      sha256 "18056981bd5517ddf308deea8138e69372d0cb415db50eb9aefbadd75688f281"
+      sha256 "e6420641a5990732b3e46a9ac16f5eaba524a67452285bb73f246a4fb286215d"
     end
     on_intel do
       url "https://github.com/kirby88/vix-releases/releases/download/v0.3.1/vix-linux-amd64.tar.gz"
-      sha256 "f19f181ad236c858ac8ac895537f80f8d2f7aa573fb19317296345b932ea147a"
+      sha256 "1f99825e595047ff494dcd6e7dcde1614c945bc73a4350f2f5d4b6cf18aa936f"
     end
   end
 
